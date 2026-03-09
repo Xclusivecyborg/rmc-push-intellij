759e871408db84d04c7555e1be36b403587a11a8
Ayodeji Ogundairo <Xclusivecyborg@gmail.com>
2026-03-09 09:00:13 +0100

    build: update plugin signing configuration
    Update the `signPlugin` task in `build.gradle.kts` to use the `password`
    property instead of the deprecated `privateKeyPassword` for signing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ tasks {
     signPlugin {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
-        privateKeyPassword.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
     }
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
